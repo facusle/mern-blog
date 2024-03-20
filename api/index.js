bf9f385560ea2e199f5ea9c1ef037a6c411ee74d
duonglt95 <duong.le@screen1.io>
2024-03-20 11:15:25 +0700

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+     .connect(process.env.MONGO)
+     .then(() => {
+          console.log('mongodb is connected');
+     })
+     .catch((err) => {
+          console.log('bug roi may oi: ', err);
+     });
 
 const app = express();
 
