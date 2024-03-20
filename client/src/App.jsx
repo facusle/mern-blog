47cb50258d243d3a9beadf87cfcb9ed8d6964312
duonglt95 <duong.le@screen1.io>
2024-03-20 10:48:51 +0700

    setup expressjs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Project from './pages/Project';
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route path="/signin" element={<Signin />}></Route>
-                <Route path="/signup" element={<Signup />}></Route>
-                <Route path="/dashboard" element={<Dashboard />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+     return (
+          <BrowserRouter>
+               <Header />
+               <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/about" element={<About />}></Route>
+                    <Route path="/signin" element={<Signin />}></Route>
+                    <Route path="/signup" element={<Signup />}></Route>
+                    <Route path="/dashboard" element={<Dashboard />}></Route>
+                    <Route path="/projects" element={<Project />}></Route>
+               </Routes>
+          </BrowserRouter>
+     );
 }
