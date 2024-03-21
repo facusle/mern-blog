ab32d3fa46257e737376950064be436aa4095598
duonglt95 <duong.le@screen1.io>
2024-03-21 17:41:23 +0700

    install persist for store data to local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import useReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-     reducer: {
-          user: useReducer,
-     },
+const rootReducers = combineReducers({
+     user: useReducer,
 });
+
+const persistConfig = {
+     key: 'root',
+     storage,
+     version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+     reducer: persistedReducer,
+     middleware: (GetDefaultMiddleware) => GetDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
