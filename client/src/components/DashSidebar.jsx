d656d02696c15cf2e841b59de68f0c15443368c3
duonglt95 <duong.le@screen1.io>
2024-03-25 17:38:06 +0700

    update Dashboard, dash sidebar

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { Sidebar } from 'flowbite-react';
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+
+export default function DashSidebar(data) {
+     const { tab } = data ? data : false;
+     function handleOpenTab(id) {
+          console.log(id);
+     }
+     return (
+          <div className="my-5 md:my-10">
+               <Sidebar aria-label="Sidebar with logo branding example">
+                    <div className="flex p-2 mb-3 text-lg font-bold border-b border-gray-950 dark:border-white">
+                         <svg
+                              className="w-6 h-6 text-gray-800 dark:text-white"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="24"
+                              height="24"
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                         >
+                              <path
+                                   fillRule="evenodd"
+                                   d="M5.005 10.19a1 1 0 0 1 1 1v.233l5.998 3.464L18 11.423v-.232a1 1 0 1 1 2 0V12a1 1 0 0 1-.5.866l-6.997 4.042a1 1 0 0 1-1 0l-6.998-4.042a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1ZM5 15.15a1 1 0 0 1 1 1v.232l5.997 3.464 5.998-3.464v-.232a1 1 0 1 1 2 0v.81a1 1 0 0 1-.5.865l-6.998 4.042a1 1 0 0 1-1 0L4.5 17.824a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1Z"
+                                   clipRule="evenodd"
+                              />
+                              <path d="M12.503 2.134a1 1 0 0 0-1 0L4.501 6.17A1 1 0 0 0 4.5 7.902l7.002 4.047a1 1 0 0 0 1 0l6.998-4.04a1 1 0 0 0 0-1.732l-6.997-4.042Z" />
+                         </svg>
+                         <p className="inline ml-2">DashBoard</p>
+                    </div>
+
+                    <Sidebar.Items>
+                         <Sidebar.ItemGroup>
+                              <Sidebar.Item
+                                   id="profile"
+                                   href="/dashboard?tab=profile"
+                                   icon={HiUser}
+                                   active={tab === 'profile'}
+                                   onClick={(e) => handleOpenTab(e.currentTarget.id)}
+                              >
+                                   Profile
+                              </Sidebar.Item>
+                              <Sidebar.Item href="#" icon={HiViewBoards}>
+                                   Kanban
+                              </Sidebar.Item>
+                              <Sidebar.Item href="#" icon={HiInbox}>
+                                   Inbox
+                              </Sidebar.Item>
+                              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                                   Products
+                              </Sidebar.Item>
+                              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                                   Sign In
+                              </Sidebar.Item>
+                              <Sidebar.Item href="#" icon={HiTable}>
+                                   Sign Up
+                              </Sidebar.Item>
+                         </Sidebar.ItemGroup>
+                    </Sidebar.Items>
+               </Sidebar>
+          </div>
+     );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
-     return <div>Dashboardgdfhgdfhfgh</div>;
+     const location = useLocation();
+     const [tab, setTab] = useState('');
+
+     useEffect(() => {
+          const urlParams = new URLSearchParams(location.search);
+          const tabFromUrl = urlParams.get('tab');
+          if (tabFromUrl) {
+               setTab(tabFromUrl);
+          }
+     }, [location.search]);
+
+     return (
+          <div>
+               <div className="">
+                    <DashSidebar tab={tab} />
+               </div>
+               {tab === 'profile' && <DashProfile />}
+          </div>
+     );
 }
